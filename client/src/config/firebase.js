d92781362ee7f77b6c5ed550249ecdbc25cda13c
Sanjana Jain <40856439+Sanjana2070@users.noreply.github.com>
2025-06-17 00:17:23 +0530

    Update firebase.js

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCxzg7wNkESD8awoiN8djOMznjQR8nVSzk",
+  apiKey: "<api-key>",
   authDomain: "qalira.firebaseapp.com",
   projectId: "qalira",
   storageBucket: "qalira.firebasestorage.app",
-  messagingSenderId: "1063790029960",
-  appId: "1:1063790029960:web:c7cdf68641f3c45ee3433d",
-  measurementId: "G-59F7K5H7BK"
+  messagingSenderId: "<sender-id>",
+  appId: "<app-id>",
+  measurementId: "<measurement-id>"
 };
 
 // Initialize Firebase
